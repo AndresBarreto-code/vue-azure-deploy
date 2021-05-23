030ea361268d29f15a76cdea114c18b27ec24fff
Andres Barreto <andres.barreto@koombea.com>
2021-05-23 11:42:01 -0500

    Router implemented

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'Home' }">Home</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link> |
+      <router-link :to="{ name: 'Page1' }">Page 1</router-link> |
+      <router-link :to="{ name: 'Page2' }">Page 2</router-link> |
+      <router-link :to="{ name: 'Page3' }">Page 3</router-link> 
     </div>
     <router-view/>
   </div>
 </template>
+<script>
+
+export default ({
+  
+})
+</script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,23 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/page1',
+    name: 'Page1',
+    component: () => import('../views/Page1.vue')
+  },
+  {
+    path: '/page2',
+    name: 'Page2',
+    component: () => import('../views/Page2.vue')
+  },
+  {
+    path: '/page3',
+    name: 'Page3',
+    component: () => import('../views/Page3.vue')
+  },
 ]
 
 const router = new VueRouter({
